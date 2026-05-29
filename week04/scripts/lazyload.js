9c21c3489ccf92f3f29e840a0bb8ff8e127b29fe
AeroSub7 <eric.jeremiah.johnson@gmail.com>
2026-05-29 17:19:13 -0600

    week04 lesson 2

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,4 @@
+const currentyear = document.querySelector("#currentyear");
+const year = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
+currentyear.innerHTML = `${year}`;
